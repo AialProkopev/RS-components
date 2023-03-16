e9a47a359a3993407aa897eed9c5c1c7748e8502
Ayal Prokopev <freezzforever@gmail.com>
2023-03-16 14:01:47 +0300

    feat: add react-router-dom and add basic struct

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.scss';
+import './styles/index.scss';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
+import Layout from './components/Layout';
+import MainPage from './pages/MainPage';
+import AboutPage from './pages/AboutPage';
+import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<MainPage />} />
+      <Route path="about" element={<AboutPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ErrorPage extends React.Component {
+  render() {
+    return <div>404</div>;
+  }
+}
+
+export default ErrorPage;
