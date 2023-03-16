5d055d60af98450d09e1b03fd3259724befcb77e
Ayal Prokopev <freezzforever@gmail.com>
2023-03-16 17:15:17 +0300

    feat: add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './index.scss';
+
+interface isActiveLink {
+  isActive: boolean;
+}
 
 class Header extends React.Component {
+  isActiveLink = ({ isActive }: isActiveLink) =>
+    isActive ? 'navigation__item_active' : 'navigation__item';
   render() {
-    return <header>Header</header>;
+    return (
+      <header className="header">
+        <nav className="navigation">
+          <NavLink to="/" className={this.isActiveLink}>
+            Home
+          </NavLink>
+          <NavLink to="/about" className={this.isActiveLink}>
+            About Us
+          </NavLink>
+        </nav>
+      </header>
+    );
   }
 }
 
