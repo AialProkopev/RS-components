b90fb9caab78b91a9332a7c6bb6777f8c6fc2d96
Ayal Prokopev <freezzforever@gmail.com>
2023-03-16 17:38:50 +0300

    feat: add error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 class ErrorPage extends React.Component {
   render() {
-    return <div>404</div>;
+    return (
+      <div className="error-page">
+        <div className="error-page__box">
+          <h2 className="error-page__title">404</h2>
+          <p className="error-page__message">We can&apos;t find that page</p>
+        </div>
+      </div>
+    );
   }
 }
 
