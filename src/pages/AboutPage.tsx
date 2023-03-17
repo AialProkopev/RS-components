3b6496f9322fb8c5dbc861dc9435d8e5cf257411
Ayal Prokopev <freezzforever@gmail.com>
2023-03-17 15:29:41 +0300

    feat: add content to about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 class AboutPage extends React.Component {
   render() {
-    return <div>AboutPage</div>;
+    return (
+      <div className="about-page">
+        <h2 className="about-page__title">Airbnb Clone</h2>
+        <p className="about-page__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+      </div>
+    );
   }
 }
 
