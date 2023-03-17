afa70ac1bcf2f80b54a02dc34c69e50ced155954
Ayal Prokopev <freezzforever@gmail.com>
2023-03-17 18:53:35 +0300

    feat: add some routing test

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-  Route,
-} from 'react-router-dom';
-import Layout from './components/Layout';
-import MainPage from './pages/MainPage';
-import AboutPage from './pages/AboutPage';
-import ErrorPage from './pages/ErrorPage';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index element={<MainPage />} />
-      <Route path="about" element={<AboutPage />} />
-      <Route path="*" element={<ErrorPage />} />
-    </Route>
-  )
-);
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
