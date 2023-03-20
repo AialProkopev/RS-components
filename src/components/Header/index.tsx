b50a32a43a4bb856929a21e32238c76b14308094
Ayal Prokopev <freezzforever@gmail.com>
2023-03-20 19:36:47 +0300

    fix: add header title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,16 +6,41 @@ interface isActiveLink {
 }
 
 class Header extends React.Component {
+  state: {
+    currentPage: string;
+  } = {
+    currentPage: '',
+  };
+  componentDidMount() {
+    const path = location.pathname;
+    if (path === '/') {
+      this.setState({
+        currentPage: 'Home Page',
+      });
+    } else if (path === '/about') {
+      this.setState({
+        currentPage: 'About Us Page',
+      });
+    }
+  }
+
   isActiveLink = ({ isActive }: isActiveLink) =>
     isActive ? 'navigation__item navigation__item_active' : 'navigation__item';
+
+  handleClickOnLink = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    this.setState({
+      currentPage: e.currentTarget.innerHTML + ' Page',
+    });
+  };
   render() {
     return (
       <header className="header">
+        <h2 className="header__title">{this.state.currentPage}</h2>
         <nav className="navigation">
-          <NavLink to="/" className={this.isActiveLink}>
+          <NavLink to="/" className={this.isActiveLink} onClick={this.handleClickOnLink}>
             Home
           </NavLink>
-          <NavLink to="/about" className={this.isActiveLink}>
+          <NavLink to="/about" className={this.isActiveLink} onClick={this.handleClickOnLink}>
             About Us
           </NavLink>
         </nav>
